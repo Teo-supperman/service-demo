3339bbe11372923928897bd1e0b8335004d9f806
tobi <tai.mongtri@gmail.com>
2023-04-09 20:19:55 +0700

    add DB h2

---
+++ src/main/java/com/api/service/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+/**
+ * @author tobi
+ * @Date : 9/4/2023
+ * @Time : 8:04 PM
+ */
+package com.api.service.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+}
---
+++ src/main/java/com/api/service/demo/model/Person.java
@@ -0,0 +1,10 @@
+/**
+ * @author tobi
+ * @Date : 9/4/2023
+ * @Time : 8:05 PM
+ */
+package com.api.service.demo.model;
+
+public class Person {
+
+}
